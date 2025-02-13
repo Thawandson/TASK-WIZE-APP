757260a3c4180264271ccc8f48af2bd1415a3cf3
Thawandson <thawandsoon@gmail.com>
2025-02-13 18:20:01 -0300

    Ajuste cabeçalho

--- src/app/components/cabecalho/cabecalho.module.ts
+++ src/app/components/cabecalho/cabecalho.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CabecalhoComponent } from './cabecalho.component';
-
-
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
     CabecalhoComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
   ]
 })
 export class CabecalhoModule { }
