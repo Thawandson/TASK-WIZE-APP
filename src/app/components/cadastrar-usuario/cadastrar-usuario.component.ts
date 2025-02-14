b1e9baa7af12542ad99276c2d27eb23b12579fbb
Thawandson <thawandsoon@gmail.com>
2025-02-14 17:27:45 -0300

    Criado forms cadastrar usuario

--- src/app/components/cadastrar-usuario/cadastrar-usuario.component.ts
+++ src/app/components/cadastrar-usuario/cadastrar-usuario.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import Swal from 'sweetalert2'
 
 @Component({
@@ -7,11 +8,55 @@ import Swal from 'sweetalert2'
   styleUrls: ['./cadastrar-usuario.component.scss']
 })
 export class CadastrarUsuarioComponent implements OnInit {
-
-  constructor() { }
+    cadastroForm!: FormGroup;
+    hidePassword = true;
+    hideConfirmPassword = true;
   
-  ngOnInit(): void {
-
-  }
+    constructor(private fb: FormBuilder) {}
+  
+    ngOnInit(): void {
+      this.cadastroForm = this.fb.group({
+        nome: ['', [Validators.required, Validators.minLength(3)]],
+        dataNascimento: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        senha: ['', [Validators.required, Validators.minLength(6)]],
+        confirmacaoSenha: ['', Validators.required],
+        cep: ['', [Validators.required, Validators.pattern(/^\d{5}-?\d{3}$/)]],
+        bairro: [''],
+        localidade: [''],
+        uf: ['']
+      }, { validator: this.validaSenhas });
+    }
+  
+    validaSenhas(formGroup: FormGroup) {
+      const senha = formGroup.get('senha')?.value;
+      const confirmacaoSenha = formGroup.get('confirmacaoSenha')?.value;
+      return senha === confirmacaoSenha ? null : { senhaDiferente: true };
+    }
+  
+    buscarCep() {
+      const cep = this.cadastroForm.get('cep')?.value.replace(/\D/g, '');
+      if (cep.length === 8) {
+        fetch(`https://viacep.com.br/ws/${cep}/json/`)
+          .then(response => response.json())
+          .then(data => {
+            if (!data.erro) {
+              this.cadastroForm.patchValue({
+                bairro: data.bairro,
+                localidade: data.localidade,
+                uf: data.uf,             
+              });
+            }
+          });
+      }
+    }
+  
+    onSubmit() {
+      if (this.cadastroForm.valid) {
+        console.log('Dados enviados:', this.cadastroForm.value);
+      } else {
+        console.log('Formulário inválido!');
+      }
+    }
 
 }
--- src/app/components/cadastrar-usuario/cadastrar-usuario.module.ts
+++ src/app/components/cadastrar-usuario/cadastrar-usuario.module.ts
@@ -4,6 +4,11 @@ import { CadastrarUsuarioComponent } from './cadastrar-usuario.component';
 import { CadastrarUsuarioRoutingModule } from './cadastrar-usuario-routing.module';
 import { CabecalhoModule } from '../cabecalho/cabecalho.module';
 import { RodapeModule } from '../rodape/rodape.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -13,7 +18,12 @@ import { RodapeModule } from '../rodape/rodape.module';
     CommonModule,
     CadastrarUsuarioRoutingModule,
     CabecalhoModule,
-    RodapeModule
+    RodapeModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule
   ]
 })
 export class CadastrarUsuarioModule { }
