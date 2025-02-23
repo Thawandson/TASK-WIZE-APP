9b2d57e25de664ad6eed3a5e42bf745364936252
Thawandson <thawandsoon@gmail.com>
2025-02-23 16:41:18 -0300

    Ajuste form login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+
+  loginForm!: FormGroup;
+  hidePassword = true;
+
+  constructor(
+    private fb: FormBuilder,
+  ) { } 
+
+  ngOnInit(): void {    
+    this.loginForm = this.fb.group({
+      usuario: ["",Validators.required,],
+      senha: ['', [Validators.required, Validators.minLength(6)]],
+    })  
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log("Login Ok");
+    }else {
+      console.log("errado");
+    }
+  }
+
 
 }
--- src/app/components/login/login.module.ts
+++ src/app/components/login/login.module.ts
@@ -4,6 +4,11 @@ import { LoginComponent } from './login.component';
 import { LoginRoutingModule } from './login-routing.module';
 import { CabecalhoModule } from "../cabecalho/cabecalho.module";
 import { RodapeModule } from "../rodape/rodape.module";
+import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -13,7 +18,12 @@ import { RodapeModule } from "../rodape/rodape.module";
     CommonModule,
     LoginRoutingModule,
     CabecalhoModule,
-    RodapeModule
+    RodapeModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule
 ]
 })
 export class LoginModule { }
