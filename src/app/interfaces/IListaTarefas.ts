d7dd3fdff87240a3296aaba5bee20f330ee68b45
Thawandson <thawandsoon@gmail.com>
2025-02-25 20:30:32 -0300

    Criado service para lista de tarefaas

--- src/app/interfaces/IListaTarefas.ts
+++ src/app/interfaces/IListaTarefas.ts
@@ -2,5 +2,8 @@ export interface IListaTarefas{
     id?: string;    
     idUsuario: string;
     tituloTarefa:string;
+    dataInicio:string;
+    dataFInal?:string;
+    status:string;
     descricao: string; 
 }
